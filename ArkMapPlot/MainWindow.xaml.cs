0639a42698601522ae72c62b7c7472b809fbf305
shadowmage45 <shadowmage45@hotmail.com>
2017-06-19 15:22:14 -0600

    add functional pan+zoom to map display add functional plotting of dino locations to display; needs refinement/polish --need to add map-pin tooltip support

--- ArkMapPlot/MainWindow.xaml.cs
+++ ArkMapPlot/MainWindow.xaml.cs
@@ -14,8 +14,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Newtonsoft.Json.Linq;
 using System.IO;
-using System.Drawing;
-using System.Drawing.Imaging;
 
 namespace ArkMapPlot
 {
@@ -29,12 +27,29 @@ namespace ArkMapPlot
         private Dictionary<string, string> displayToClass = new Dictionary<string, string>();
 
         private List<MemberData> displayedMembers = new List<MemberData>();
+        private MemberData selectedMember = null;
+        private Dictionary<MemberData, Image> pins = new Dictionary<ArkMapPlot.MemberData, Image>();
+
+        public static string pinRedPath = "pin_red.png";
+        public static string pinBluePath = "pin_blue.png";
+        public static BitmapImage pinRedImage;
+        public static BitmapImage pinBlueImage;
+        public static string logFile = "log.txt";
 
         public MainWindow()
         {
-            loadData();
-            InitializeComponent();
-            populateWindow();
+            StreamWriter sw = new StreamWriter(new FileStream(logFile, FileMode.OpenOrCreate));
+            System.Console.SetOut(sw);
+            try
+            {
+                loadData();
+                InitializeComponent();
+                populateWindow();
+            }
+            catch (Exception e)
+            {
+                print(e.StackTrace);
+            }
         }
 
         private void loadData()
@@ -80,6 +95,9 @@ namespace ArkMapPlot
                     //print("Did not locate any file for: " + fileName);
                 }
             }
+
+            pinRedImage = loadImage(pinRedPath);
+            pinBlueImage = loadImage(pinBluePath);
         }
 
         private void populateWindow()
@@ -92,31 +110,40 @@ namespace ArkMapPlot
             {
                 updateMemberList((string)ClassList.SelectedItem);
                 updateMemberData(null);
-                //print("Selected class: " + ClassList.SelectedItem);
+                string cName = displayToClass[(string)ClassList.SelectedItem];
+                selectedMember = null;
+                updateMapPins(classData[cName]);
+                print("Selected class: " + ClassList.SelectedItem);
             };
             MemberData.SelectionChanged += delegate (object sender, SelectionChangedEventArgs e)
             {
                 string cName = (string)ClassList.SelectedItem;
                 MemberData member = (MemberData)MemberData.SelectedItem;
+                selectedMember = member;
                 updateMemberData(member);
-                if (member != null)
-                {
-                    //print("Selected member: " + member.DisplayName + " of class: " + cName);
-                }
-                MapImage.ToolTip = member.DisplayName;
+                updateMapPins(classData[displayToClass[cName]]);
+                print("Selected member: " + member);
             };
-            MapImage.MouseMove += delegate (object sender, MouseEventArgs e) 
-            {
-                System.Windows.Point pt = e.GetPosition(MapImage);
-                print("Move-Pixels: " + pt);
-            };
-            MapImage.MouseLeftButtonDown += delegate (object sender, MouseButtonEventArgs e)
-            {
-                System.Windows.Point pt = e.GetPosition(MapImage);
-                print("Clck-Pixels: " + pt);
-            };
+            //MapImage.MouseMove += delegate (object sender, MouseEventArgs e)
+            //{   
+            //    System.Windows.Point pt = e.GetPosition(MapImage);
+            //    Point pt1 = e.GetPosition(MapCanvas);
+            //};
+            //MapImage.MouseLeftButtonDown += delegate (object sender, MouseButtonEventArgs e)
+            //{
+            //    System.Windows.Point pt = e.GetPosition(MapImage);
+            //    print("Clck-Pixels: " + pt);
+            //};
+            //MapImage.MouseWheel += delegate (object sender, MouseWheelEventArgs e)
+            //{
+            //    print("Wheel: " + e.Delta);
+            //    //TransformGroup tg = MapCanvas.tra
+            //    //var rt = MapCanvas.RenderTransform;
+            //    //((ScaleTransform)rt).ScaleX += e.Delta;
+            //    //((ScaleTransform)rt).ScaleY += e.Delta;
+            //    //MapCanvas.RenderTransform = rt;
+            //};
             loadMap(string.Empty);
-            MapImage.ToolTip = "Foobar";
         }
 
         private void loadMap(string configFileName)
@@ -150,7 +177,41 @@ namespace ArkMapPlot
 
         private void updateMapPins(ClassData data)
         {
-
+            MapCanvas.Children.RemoveRange(1, MapCanvas.Children.Count - 1);
+            int len = data.members.Count;
+            MemberData mData;
+            float x, y;
+            float width = 1430;
+            float height = 1430;
+            float latMax = 100;
+            float lonMax = 100;
+            for (int i = 0; i < len; i++)
+            {
+                mData = data.members[i];
+                Image pin = new Image();
+                pin.IsHitTestVisible = false;
+                if (mData == selectedMember) { continue; }
+                pin.Source = pinBlueImage;
+                x = (mData.Lon / lonMax) * width - 16;
+                y = (mData.Lat / latMax) * height - 16;
+                MapCanvas.Children.Add(pin);
+                Canvas.SetLeft(pin, x);
+                Canvas.SetTop(pin, y);
+                print("Added Pin at: "+x+","+y);
+            }
+            if (selectedMember != null)
+            {
+                mData = selectedMember;
+                Image pin = new Image();
+                pin.IsHitTestVisible = false;
+                pin.Source = pinRedImage;
+                x = (mData.Lon / lonMax) * width - 16;
+                y = (mData.Lat / latMax) * height - 16;
+                MapCanvas.Children.Add(pin);                
+                Canvas.SetLeft(pin, x);
+                Canvas.SetTop(pin, y);
+                print("Added Selected Member Pin at: " + x + "," + y);
+            }
         }
 
         private BitmapImage loadImage(string file)
@@ -171,11 +232,13 @@ namespace ArkMapPlot
         public static void print(string line)
         {
             Console.Out.WriteLine(line);
+            Console.Out.Flush();
         }
 
         public static void print(object var)
         {
             Console.Out.WriteLine(var == null ? "null" : var.ToString());
+            Console.Out.Flush();
         }
     }
 
@@ -235,6 +298,10 @@ namespace ArkMapPlot
         public int Damage { get { return damage; } }
         public int Weight { get { return weight; } }
         public int Speed { get { return speed; } }
+        public float Lat { get { return lat; } }
+        public float Lon { get { return lon; } }
+        public float X { get { return x; } }
+        public float Y { get { return y; } }
 
         public MemberData(string className)
         {
@@ -310,4 +377,142 @@ namespace ArkMapPlot
             block.Text = data;
         }
     }
+    
+    public class ZoomBorder : Border
+    {
+        private UIElement child = null;
+        private Point origin;
+        private Point start;
+
+        private TranslateTransform GetTranslateTransform(UIElement element)
+        {
+            return (TranslateTransform)((TransformGroup)element.RenderTransform)
+              .Children.First(tr => tr is TranslateTransform);
+        }
+
+        private ScaleTransform GetScaleTransform(UIElement element)
+        {
+            return (ScaleTransform)((TransformGroup)element.RenderTransform)
+              .Children.First(tr => tr is ScaleTransform);
+        }
+
+        public override UIElement Child
+        {
+            get { return base.Child; }
+            set
+            {
+                if (value != null && value != this.Child)
+                    this.Initialize(value);
+                base.Child = value;
+            }
+        }
+
+        public void Initialize(UIElement element)
+        {
+            this.child = element;
+            if (child != null)
+            {
+                TransformGroup group = new TransformGroup();
+                ScaleTransform st = new ScaleTransform();
+                group.Children.Add(st);
+                TranslateTransform tt = new TranslateTransform();
+                group.Children.Add(tt);
+                child.RenderTransform = group;
+                child.RenderTransformOrigin = new Point(0.0, 0.0);
+                this.MouseWheel += child_MouseWheel;
+                this.MouseLeftButtonDown += child_MouseLeftButtonDown;
+                this.MouseLeftButtonUp += child_MouseLeftButtonUp;
+                this.MouseMove += child_MouseMove;
+                this.PreviewMouseRightButtonDown += new MouseButtonEventHandler(
+                  child_PreviewMouseRightButtonDown);
+            }
+        }
+
+        public void Reset()
+        {
+            if (child != null)
+            {
+                // reset zoom
+                var st = GetScaleTransform(child);
+                st.ScaleX = 1.0;
+                st.ScaleY = 1.0;
+
+                // reset pan
+                var tt = GetTranslateTransform(child);
+                tt.X = 0.0;
+                tt.Y = 0.0;
+            }
+        }
+
+        #region Child Events
+
+        private void child_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (child != null)
+            {
+                var st = GetScaleTransform(child);
+                var tt = GetTranslateTransform(child);
+
+                double zoom = e.Delta > 0 ? .2 : -.2;
+                if (!(e.Delta > 0) && (st.ScaleX < .4 || st.ScaleY < .4))
+                    return;
+
+                Point relative = e.GetPosition(child);
+                double abosuluteX;
+                double abosuluteY;
+
+                abosuluteX = relative.X * st.ScaleX + tt.X;
+                abosuluteY = relative.Y * st.ScaleY + tt.Y;
+
+                st.ScaleX += zoom;
+                st.ScaleY += zoom;
+
+                tt.X = abosuluteX - relative.X * st.ScaleX;
+                tt.Y = abosuluteY - relative.Y * st.ScaleY;
+            }
+        }
+
+        private void child_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (child != null)
+            {
+                var tt = GetTranslateTransform(child);
+                start = e.GetPosition(this);
+                origin = new Point(tt.X, tt.Y);
+                this.Cursor = Cursors.Hand;
+                child.CaptureMouse();
+            }
+        }
+
+        private void child_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (child != null)
+            {
+                child.ReleaseMouseCapture();
+                this.Cursor = Cursors.Arrow;
+            }
+        }
+
+        void child_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Reset();
+        }
+
+        private void child_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (child != null)
+            {
+                if (child.IsMouseCaptured)
+                {
+                    var tt = GetTranslateTransform(child);
+                    Vector v = start - e.GetPosition(this);
+                    tt.X = origin.X - v.X;
+                    tt.Y = origin.Y - v.Y;
+                }
+            }
+        }
+
+        #endregion
+    }
+
 }
